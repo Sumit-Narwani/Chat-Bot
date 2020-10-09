c0b743f451d04c9befe3bbb8c5d878f336664ac2
Sumit Narwani <narwani95@gmail.com>
2020-10-09 19:11:52 +0530

    Fixed request protocol conflict

--- script.js
+++ script.js
@@ -190,14 +190,14 @@ class ChatTree {
 }
 
 async function getJoke() {
-  const response = await fetch("http://api.icndb.com/jokes/random");
+  const response = await fetch("https://api.icndb.com/jokes/random");
   const jsonResp = await response.json();
   return jsonResp;
 }
 
 async function getNews() {
   const response = await fetch(
-    "http://newsapi.org/v2/top-headlines?country=in&pageSize=1&apiKey=a876816f98574cdfa23ffdc7d531c7bc"
+    "https://newsapi.org/v2/top-headlines?country=in&pageSize=1&apiKey=a876816f98574cdfa23ffdc7d531c7bc"
   );
   const jsonResp = await response.json();
   return jsonResp;
